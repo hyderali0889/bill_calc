3b523b32275f0935796dc91521c7e4969a0b118a
Hayder Ali <hyderali0889@gmail.com>
2022-03-10 15:09:19 +0500

    first Commit

---
+++ android/app/src/main/kotlin/com/example/bill_calc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bill_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/UI/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BillCalc extends StatefulWidget {
+  const BillCalc({ Key? key }) : super(key: key);
+
+  @override
+  State<BillCalc> createState() => _BillCalcState();
+}
+
+class _BillCalcState extends State<BillCalc> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
